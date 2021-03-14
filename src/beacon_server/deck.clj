133dc4b5f7aeff9ae5ad7308916344957d2ed79e
Markus Hjort <markus.hjort@gmail.com>
2021-03-14 13:55:53 +0200

    Add helper function for providing cards with same suit to all players

--- src/beacon_server/deck.clj
+++ src/beacon_server/deck.clj
@@ -10,14 +10,17 @@
                                 13 [13 "K" 4]
                                 14 [15 "A" 11]
                                 [i (str i) 0]))
-        cards (for [value (range 6 15)
-                    suit [:hearts :clubs :spades :diamonds]]
+        cards (for [suit [:hearts :clubs :spades :diamonds]
+                    value (range 6 15)]
                 (let [[final-value text points] (card-value-text-and-points value)]
                   {:suit suit :value final-value :text text :points points}))]
     cards))
 
 (defn shuffle-for-four-players [deck]
   (partition 9 (shuffle deck)))
+
+(defn same-suit-for-four-players [deck]
+  (partition 9 deck))
 
 (defn print-card [{:keys [text suit]}]
   (str text (string/upper-case (apply str (take 1 (name suit))))))
