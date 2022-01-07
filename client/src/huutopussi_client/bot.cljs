3b2371ccc01856d002028c4ccf89651fd86a3b55
Markus Hjort <markus.hjort@gmail.com>
2022-01-07 15:53:48 +0200

    Add random possible action functionality for bots

--- client/src/huutopussi_client/bot.cljs
+++ client/src/huutopussi_client/bot.cljs
@@ -9,6 +9,11 @@
     (println "BOT: Playing first card" first-possible-card)
     [:player-card (index-of first-possible-card hand-cards)]))
 
+(defn- run-random-possible-action [possible-actions]
+  (let [action (first (shuffle possible-actions))]
+    (println "BOT: Playing random action" action)
+    [:player-action action]))
+
 (defn- run-action-with-first-possible-value [{:keys [possible-values id]}]
   (let [first-possible-value (first possible-values)]
     (println "BOT: Playing action" id "with first possible value" first-possible-value)
@@ -20,11 +25,16 @@
     [:player-action fold-action]
     (run-action-with-first-possible-value (first possible-actions))))
 
-(defn choose-bot-action [{:keys [phase hand-cards possible-cards possible-actions]}]
+(defn- choose-marjapussi-action [{:keys [hand-cards possible-cards possible-actions]}]
+  (if (seq possible-actions)
+    (run-random-possible-action possible-actions)
+    (when (seq possible-cards)
+      (play-first-possible-card hand-cards possible-cards))))
+
+(defn choose-bot-action [{:keys [phase possible-actions] :as params}]
   (case phase
     "bidding" (choose-bidding-action possible-actions)
-    "marjapussi" (when (seq possible-cards)
-                   (play-first-possible-card hand-cards possible-cards))))
+    "marjapussi" (choose-marjapussi-action params)))
 
 (comment
   ;Starting player
@@ -37,4 +47,16 @@
   ;Player who won the bidding
   (choose-bot-action {:phase "bidding" :possible-actions [{:id "set-target-score"
                                                            :possible-values [50 100]}]})
+
+  ;Player with possible trump actions
+  (choose-bot-action {:phase "marjapussi"
+                      :possible-actions  [{:id "ask-for-trump" :action-type :ask-for-trump :target-player "c"}
+                                          {:id "ask-for-half-trump:hearts"
+                                           :action-type :ask-for-half-trump
+                                           :suit :hearts
+                                           :target-player "c"}
+                                          {:id "ask-for-half-trump:clubs"
+                                           :action-type :ask-for-half-trump
+                                           :suit :clubs
+                                           :target-player "c"}]})
   )
