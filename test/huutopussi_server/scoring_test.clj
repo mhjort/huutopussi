7d8cb2e59ec0dd6e27ec2f531f83c4df75046fce
Markus Hjort <markus.hjort@gmail.com>
2022-06-02 13:49:04 +0300

    Add function for calculating the winning team

--- test/huutopussi_server/scoring_test.clj
+++ test/huutopussi_server/scoring_test.clj
@@ -1,5 +1,5 @@
 (ns huutopussi-server.scoring-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing]]
             [huutopussi-server.scoring :as scoring]))
 
 (def teams {:Team1 {:players ["a" "c"] :total-score 100}
@@ -16,3 +16,15 @@
           :Team2 {:players ["b" "d"] :total-score 0}}
          (scoring/update-team-scores teams {:Team1 {:current 50}
                                             :Team2 {:current 90 :target 100}}))))
+
+(deftest get-winning-team
+  (let [teams {:Team1 {:players ["a" "c"] :total-score 120}
+               :Team2 {:players ["b" "d"] :total-score 150}}]
+    (testing "Returns nil when non of the teams has enough points"
+      (is (= nil (scoring/get-winning-team teams 200))))
+    (testing "Returns the team that has enough points to win"
+      (is (= :Team2 (scoring/get-winning-team teams 150))))
+    (testing "Returns the team with more points when both teams have enough points"
+      (is (= :Team2 (scoring/get-winning-team teams 120))))
+    (testing "Returns nil when both teams have exactly the same score and have enough points"
+      (is (= nil (scoring/get-winning-team (assoc-in teams [:Team1 :total-score] 150) 150))))))
