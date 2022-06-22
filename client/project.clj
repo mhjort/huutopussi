b98507ada6afc39c20cd2fa9d78ea98935c795c3
Markus Hjort <markus.hjort@gmail.com>
2022-06-22 10:36:54 +0300

    Bump dependencies
    Note! Reagent 1.11 does not automatically depend on react libs.
    Therefore these are explicit dependencies now.

--- client/project.clj
+++ client/project.clj
@@ -6,13 +6,15 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.773"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.57"]
                  [cljs-http "0.1.46"]
                  [com.cemerick/url "0.1.1"]
-                 [org.clojure/core.async "1.3.610"]
-                 [reagent "1.0.0-alpha2"]
-                 [re-frame "1.1.1"]]
+                 [org.clojure/core.async "1.5.648"]
+                 [reagent "1.1.1"]
+                 [cljsjs/react "17.0.2-0"]
+                 [cljsjs/react-dom "17.0.2-0"]
+                 [re-frame "1.2.0"]]
 
   :source-paths ["src"]
   :resource-paths ["resources" "target"]
@@ -22,7 +24,7 @@
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "huutopussi-beacon.test-runner"]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.9"]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.18"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
                    }})
 
--- project.clj
+++ project.clj
@@ -2,23 +2,23 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
-                 [medley "1.3.0"]
-                 [metosin/malli "0.6.2"]
-                 [org.clojure/tools.logging "1.1.0"]
-                 [org.clojure/core.async "1.3.610"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [compojure "1.7.0"]
+                 [medley "1.4.0"]
+                 [metosin/malli "0.8.4"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.clojure/core.async "1.5.648"]
                  [org.clojure/math.combinatorics "0.1.6"]
-                 [ring/ring-defaults "0.3.2"]
-                 [ring/ring-json "0.5.0"]
+                 [ring/ring-defaults "0.3.3"]
+                 [ring/ring-json "0.5.1"]
                  [ring-cors "0.1.13"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler huutopussi-server.handler/prod-app}
   :uberjar-name "huutopussi-standalone.jar"
-  :profiles {:reveal {:dependencies [[nrepl "0.8.3"]
+  :profiles {:reveal {:dependencies [[nrepl "0.9.0"]
                                      [nrepl-complete "0.1.0"]
-                                     [vlaaad/reveal "1.2.186"]]
+                                     [vlaaad/reveal "1.3.273"]]
                       :repl-options {:nrepl-middleware [vlaaad.reveal.nrepl/middleware]}}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-jetty-adapter "1.8.2"]
-                                  [ring/ring-mock "0.3.2"]]}})
+                                  [ring/ring-jetty-adapter "1.9.5"]
+                                  [ring/ring-mock "0.4.0"]]}})
