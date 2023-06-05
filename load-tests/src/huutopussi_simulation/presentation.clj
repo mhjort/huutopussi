e785595727c538bd51bca7493dded22a592b1f76
Markus Hjort <markus.hjort@gmail.com>
2023-06-05 20:48:32 +0300

    Add separate notes

--- load-tests/src/huutopussi_simulation/presentation.clj
+++ load-tests/src/huutopussi_simulation/presentation.clj
@@ -1,10 +1,30 @@
 (ns huutopussi-simulation.presentation)
 
-;My name is Markus Hjort - info about who I am and what I do ~2 mins
+;; Me
+;; --
 
-;~5 mins
+;; Markus Hjort: A coder from Helsinki, Finland
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ; What is Load testing?
+; ---------------------
+
+
 
 ; LT = Load Testing tool
 ; SUT = System Under Test
@@ -17,34 +37,11 @@
 ;   \ /                            \ /
 ;    V                              V
 ;
-; Results                        Performance
-;                                metrics (APM)
-
-;Load testing basic. Call one endpoint and see the results
-;Implementation using clj-gatling both sync and async version
-
-;Tested that it takes 15-20 mins to here
-
-;~5 mins
-;Stateful applications. How to handle state in load testing?
-;Show the huutopussi match make example
+; Benchmark                      Performance
+; Results                        metrics (APM)
+;
 
 
-;~5 mins
-;Let's try to test that with the help of clj-gatling using hard coded simulation
+;; Idea is to simulate how concurrent users use the SUT
 
-;~5 mins
-;Let's try to test that with the help of clj-gatling using dynamic simulation
-
-
-
-
-
-
-
-
-
-
-
-;Stretch goals
-;With AWS Lambda (10 mins). But needs improvements to clojider
+;; And see how it behaves under the load
